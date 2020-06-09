6935289695377bf8edebd86171bc934f46246e1d
smzdm <smzdm@likefandeMacBook-Pro.local>
2020-06-09 19:11:47 +0800

    feat:基本构建

---
+++ src/page/home/index.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { Button } from 'antd';
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <div>回到home</div>
+        <Button type="primary">Button</Button>
+        <Button type="primary">Button</Button>
+        <Button type="primary">Button</Button>
+      </div>
+    );
+  }
+}
